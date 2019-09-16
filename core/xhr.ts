7d73f2c53be82482f771423eeeb7fc6c9ef9d174
linhao <linhao@galasports.net>
2019-09-16 14:41:39 +0800

    [build] 0.0.9

--- core/xhr.ts
+++ core/xhr.ts
@@ -112,7 +112,12 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         cancelToken.promise.then(reason => {
           request.abort()
           reject(reason)
-        })
+        }).catch(
+          // istanbul ignore next
+          err => {
+
+          }
+        )
       }
     }
 
